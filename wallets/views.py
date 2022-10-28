6082f81c24be427cb2233bc3a3e3245e0913c60b
Spotty Banana <spotty.banana@tutanota.com>
2022-10-28 14:03:39 +0200

    minor changes

--- wallets/views.py
+++ wallets/views.py
@@ -16,24 +16,28 @@ def index(request):
         user = WalletUser.objects.get(username=request.user.username)
 
         return render(request, 'wallet.html', {
-                'user': user
-            })
+            'user': user
+        })
 
     return render(request, 'nonlogged_index.html')
 
 
 def create(request):
-    generated_password = base64.urlsafe_b64encode(os.urandom(16)).replace(b'=', b'')
-    generated_username = b"satoshi-" + base64.urlsafe_b64encode(os.urandom(5)).replace(b'=', b'')
-    new_user = WalletUser.objects.create_user(username=generated_username, 
-        password=generated_password)
+    generated_password = base64.urlsafe_b64encode(
+        os.urandom(16)).replace(b'=', b'')
+    generated_username = b"satoshi-" + \
+        base64.urlsafe_b64encode(os.urandom(5)).replace(b'=', b'')
+    new_user = WalletUser.objects.create_user(username=generated_username,
+                                              password=generated_password)
     new_user.save()
 
-    user = authenticate(username=new_user.username, password=generated_password)
+    user = authenticate(username=new_user.username,
+                        password=generated_password)
     if user is not None:
         login(request, user)
 
-        messages.add_message(request, messages.INFO, 'Username %s Password %s' % (new_user.username, generated_password))
+        messages.add_message(request, messages.INFO, 'Username %s Password %s' % (
+            new_user.username, generated_password))
 
         return redirect("/")
 
